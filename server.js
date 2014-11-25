a2642a3130ac075c5e3640dad1bdf5ebd8e22bec
sangallimarco <sangallimarco@gmail.com>
2014-11-25 17:19:23 +0000

    initial

---
+++ server.js
@@ -0,0 +1,63 @@
+// BASE SETUP
+// =============================================================================
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/testapp');
+
+// call the packages we need
+var express    = require('express'); 		// call express
+var app        = express(); 				// define our app using express
+var bodyParser = require('body-parser');
+
+var Test     = require('./models/test');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8888; 		// set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); 				// get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+router.route('/test')
+	// create (accessed at POST http://localhost:8080/api/test)
+	.post(function(req, res) {
+		var test = new Test(); 		// create a new instance of the Bear model
+		test.name = req.body.name;  // set the bears name (comes from the request)
+
+		// save the bear and check for errors
+		test.save(function(err) {
+			if (err)
+				res.send(err);
+			res.json({ message: 'created!' });
+		});
+	})
+	.get(function(req, res) {
+		Test.find(
+			{'name': /^ma/i},
+			'name',
+			function (err, tests) {
+			if (err)
+				res.send(err);
+			res.json(tests);
+		})
+
+	});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
