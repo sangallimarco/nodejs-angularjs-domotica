1321e0b0e0e9b5c4dcb7f0f7c2b1c1c7c25b6dcb
Marco Sangalli <sangalli.marco@gmail.com>
2014-12-09 23:26:56 +0000

    adding npm config

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ pongular.module('nodejs', ['libs'])
 		app.set('view engine', 'ejs');
 		app.set('port', process.env.PORT || 3000);
 		app.use($express.static($path.join(__dirname, 'public')));
+		app.use($express.static($path.join(__dirname, 'bower_components')));
 
 		return app;
 	}
