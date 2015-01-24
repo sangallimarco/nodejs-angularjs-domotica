a6ca199a5e8bcfbb2c9e06aea64e50a57d06dc62
Marco Sangalli <sangalli.marco@gmail.com>
2015-01-24 19:52:01 +0000

    adding clean listeners for socket.io

--- public/src/js/socket/services/socketIoFactory.js
+++ public/src/js/socket/services/socketIoFactory.js
@@ -1,6 +1,26 @@
 angular.module('app.socket')
-.factory('socketIoFactory', ['socketFactory', 
-	function (socketFactory) {
-		return socketFactory();
+.factory('socketIoFactory', ['socketFactory', '$rootScope',
+	function (socketFactory, $rootScope) {
+
+		var socket = socketFactory(),
+			events = [],
+			factory = {};
+
+		// if state changed remove listeners
+		$rootScope.$on('$stateChangeStart', function(event){
+			angular.forEach(events, function (evt) {
+				socket.removeListener(evt);
+			})
+		});
+
+		factory.getSocket = function () {
+			return socket;
+		};
+		factory.addListener = function (event, callback) {
+			socket.on(event, callback);
+			events.push(event);
+		};
+
+		return factory;
 	}
 ]);
