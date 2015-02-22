1b01f29ce75269019fe5862983bc83960b234f1c
Marco Sangalli <sangalli.marco@gmail.com>
2015-02-22 22:19:04 +0000

    adding emit/on

--- public/src/js/bootstrap/directives/navigation.js
+++ public/src/js/bootstrap/directives/navigation.js
@@ -1,66 +1,71 @@
 angular.module('app.bootstrap')
-    .directive('navigationContainer', function ($rootScope) {
-        return {
-            restrict: 'E',
-            replace: true,
-            template: '<div ng-class="{open: !show}" id="navbar" class="navbar-collapse slide-right collapse"><ul class="nav navbar-nav" ng-transclude></ul></div>',
-            scope: {
-                show: '='
-            },
-            transclude: true,
-            controller: function ($scope, $element, $attrs) {
-                var self = this;
+/**
+ * Page canvas
+ */
+.directive('navigationCanvas', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        transclude: true,
+        template: '<div class="canvas" ng-class="{open: show}" ng-transclude></div>',
+        link: function (scope, element, attrs) {
+            scope.show = false;
 
-                self.toggle = function (status) {
-                    // use status to force it!!!
-                    $scope.show = status !== undefined ? status : !$scope.show;
+            scope.$on('menuToggle',
+                function(event, data){
+                    scope.show = data.status;
+                }
+            );
+        }
+    };
+})
+/**
+ * Navigation container
+ * bind variable to show/hide menu
+ */
+.directive('navigationContainer', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div ng-class="{open: !show}" id="navbar" class="navbar-collapse slide-right collapse"><ul class="nav navbar-nav" ng-transclude></ul></div>',
+        scope: {
+            show: '='
+        },
+        transclude: true,
+        link: function (scope, element, attrs) {
 
-                    $rootScope.$broadcast('menuToggle',{show: $scope.show});
+            scope.$on('menuToggle',
+                function(event, data){
+                    scope.show = data.status;
+                }
+            );
 
-                };
+            // watch variable
+            scope.$watch('show', function(value){
+                scope.$emit('menuToggle', {status: value});
+            });
+        }
+    };
+})
+/**
+ * Navigation Item
+ * Force menu to close once selected
+ */
+.directive('navigationItem', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<li ui-sref-active="active"><a ng-click="collapse()" ui-sref="{{state}}">{{label}}</a></li>',
+        scope: {
+            state: '@state',
+            label: '@label'
+        },
+        link: function (scope, element, attrs) {
 
-                $scope.$watch('show',
-                    function(newValue, oldValue){
-                        if (newValue !== undefined) {
-                            self.toggle(newValue);
-                        }
-                    }
-                );
-            }
-        };
-    })
-    .directive('navigationItem', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            template: '<li ui-sref-active="active"><a ng-click="collapse()" ui-sref="{{state}}">{{label}}</a></li>',
-            require:['^navigationContainer'],
-            scope: {
-                state: '@state',
-                label: '@label'
-            },
-            link: function (scope, element, attrs, ctrls) {
-                var navigation = ctrls[0];
-
-                scope.collapse = function () {
-                    navigation.toggle(false);
-
-                };
-            }
-        };
-    })
-    .directive('navigationCanvas', function ($rootScope) {
-        return {
-            restrict: 'E',
-            replace: true,
-            transclude: true,
-            template: '<div class="canvas" ng-class="{open: show}" ng-transclude></div>',
-            link: function (scope, element, attrs) {
-                scope.collapsed = true;
-
-                $rootScope.$on('menuToggle', function(event, data){
-                    scope.show = data.show;
-                });
-            }
-        };
-    });
+            scope.collapse = function () {
+                scope.$emit('menuToggle', {status: false});
+            };
+        }
+    };
+})
+;
